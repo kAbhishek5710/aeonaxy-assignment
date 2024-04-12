b0d643ee221f642682df52ac826948ca27b0d9eb
Abhishek Kushwaha <kabhishek5710@gmail.com>
2024-04-12 11:52:33 +0530

    Searchbar update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ export default function SearchBar() {
     <div>
       <div className="flex items-center gap-2 text-slate-400 text-xl border rounded-3xl shadow-md p-3">
         <FaSearch />
-        <input type="text" placeholder="Search our articles" />
+        <input type="text" placeholder="Search our articles" className="outline-none" />
       </div>
     </div>
   );
